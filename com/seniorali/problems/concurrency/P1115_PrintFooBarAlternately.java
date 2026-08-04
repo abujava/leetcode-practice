2a198cbae2c15ac7334b142843517582a0a35d1d
Muhammad <ali@seniorali.com>
2026-08-04 20:44:30 +0500

    1115. Print FooBar Alternately

---
+++ com/seniorali/problems/concurrency/P1115_PrintFooBarAlternately.java
@@ -0,0 +1,40 @@
+package com.seniorali.problems.concurrency;
+
+/**
+ * Problem: <a href="https://leetcode.com/problems/print-foobar-alternately">1115. Print FooBar Alternately</a><br/>
+ * <p>
+ * Difficulty: <code>Easy</code>
+ * <p>
+ * Topics: <code>Concurrency</code>
+ */
+public class P1115_PrintFooBarAlternately {
+    class FooBar {
+        private int n;
+        private boolean fooTurn;
+
+        public FooBar(int n) {
+            this.n = n;
+            fooTurn = true;
+        }
+
+        public synchronized
+        void foo(Runnable printFoo) throws InterruptedException {
+            for (int i = 0; i < n; i++) {
+                while (!fooTurn) wait();
+                printFoo.run();
+                fooTurn = false;
+                notifyAll();
+            }
+        }
+
+        public synchronized
+        void bar(Runnable printBar) throws InterruptedException {
+            for (int i = 0; i < n; i++) {
+                while (fooTurn) wait();
+                printBar.run();
+                fooTurn = true;
+                notifyAll();
+            }
+        }
+    }
+}
